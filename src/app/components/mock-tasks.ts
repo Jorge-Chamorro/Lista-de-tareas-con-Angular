6042f85ab9cd5b61a04e30e20eecd6e5c79c552d
Jorge-Chamorro <jorgeorlandochamorro@gmail.com>
2022-10-25 22:30:37 -0300

    Mock agregado

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,30 @@
+import {Task} from "./task"
+
+
+export const TASK:Task[] =[
+    {
+        id:1,
+        text: "Primera tarea", 
+        flimite: "Octubre 30 de 2022",
+        recordatorio: true
+    },
+    {
+        id:2,
+        text: "Segunda tarea", 
+        flimite: "Octubre 31 de 2022",
+        recordatorio: true
+    },
+    {
+        id:3,
+        text: "Tercera tarea", 
+        flimite: "Noviembre 30 de 2022",
+        recordatorio: false
+    },
+    {
+        id:4,
+        text: "Cuarta tarea", 
+        flimite: "Noviembre 31 de 2022",
+        recordatorio: true
+    },
+
+]
